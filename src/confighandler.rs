f08dcfaadb0e7926f8b0eb4010fbbb0120eabf24
Mark Hakansson <mark.hakansson@live.se>
2020-02-11 08:10:22 +0100

    Add config parsing

--- src/confighandler.rs
+++ src/confighandler.rs
@@ -4,36 +4,58 @@ use std::path::PathBuf;
 use std::{fs::File, io::prelude::*};
 
 use serde::Deserialize;
+use toml;
+use anyhow::{Result, anyhow};
 
-#[derive(Deserialize)]
-struct Config {
+#[derive(Debug, Clone)]
+enum ConfError {
+    
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
     api: API,
     default: Default
 }
 
-#[derive(Deserialize)]
-struct Default {
+#[derive(Deserialize, Debug)]
+pub struct Default {
     city: String,
     country: String
 }
 
-#[derive(Deserialize)]
-struct API {
+#[derive(Deserialize, Debug)]
+pub struct API {
     app_id: String
 }
 
+fn check_config_path() {}
 
-pub fn read_config() -> Result<String, io::Error> {
-    let conf_path = dirs::home_dir().unwrap().join(".wcliconf");
+/// Reads 
+pub fn read_config() -> Result<Config> {
+    let conf_path: PathBuf;
+
+     match dirs::config_dir() {
+        Some(dir) => {
+            conf_path = dir
+                        .join("weathercli")
+                        .join("wcliconf.toml")
+        },
+        None => return Err(anyhow!("Could not find the config directory in your system!"))
+    }
+
+    println!("{:#?}", conf_path);
 
     let mut file = File::open(conf_path)?;
     let mut result = String::new();
     file.read_to_string(&mut result)?;
 
-    Ok(result)
+    let conf: Config = toml::from_str(result.as_str())?;
+
+    Ok(conf)
 }
 
 pub fn init() {
-    let config = read_config().unwrap();
-    //let content = parse_config(&config);
+    let config = read_config().unwrap();    
+    println!("{:#?}", config);
 }
