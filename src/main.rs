09f5322e3b12cd598cbc210a5817c1cb55178132
Mark Hakansson <mark.hakansson@live.se>
2020-02-16 14:15:16 +0100

    Remove quicli completely

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ mod owm;
 use confighandler as config;
 
 const ZERO_CELSIUS_IN_KELVIN: f32 = 273.15;
-const HTTP_OK: u32 = 200;
 
 #[derive(Debug, StructOpt)]
 struct Cli {
@@ -22,18 +21,30 @@ struct Cli {
 
 fn main() {
     let config = config::read_config().unwrap();
-
     let app_id = config.api.app_id.as_str();
-    let location = "London,UK";
-    let url = "https://api.openweathermap.org/data/2.5/weather";
-    let query = ["q=", location, "&appid=", app_id].concat().to_string();
-
-    println!("{:#}, {:#}", url, query);
-
-    let test = owm::get_current_weather(url.to_string(), query).unwrap();
-
-    println!("{:#?}", test);
 
     let args = Cli::from_args();
-    println!("{:?}", args);
+
+    let location: String;
+    
+    match args.location {
+        None => {
+            let default = config.default.unwrap();
+            let mut res = default.city;
+            res.push_str(",");
+            res.push_str(default.country.as_str());
+            location = res;
+        },
+        Some(l) => location = l,
+    };
+
+    let url = "https://api.openweathermap.org/data/2.5/weather";
+    let query = ["q=", location.as_str(), "&appid=", app_id].concat().to_string();
+
+    let data = owm::get_current_weather(url.to_string(), query).unwrap();
+    let weather = data.weather.unwrap().pop().unwrap();
+    let main = data.main.unwrap();
+
+    println!("Current weather in {} is: {}", data.name.unwrap(), weather.description.unwrap());
+    println!("Temp: {:.1}", main.temp.unwrap() - ZERO_CELSIUS_IN_KELVIN);
 }
